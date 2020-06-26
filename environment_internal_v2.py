0299a183656f56fb4dd280166fab904176c0f83a
RHS16138 <48189026+RHS16138@users.noreply.github.com>
2020-06-26 18:33:46 +1200

    Update environment_internal_v2.py

--- environment_internal_v2.py
+++ environment_internal_v2.py
@@ -1,14 +1,23 @@
+#environment_internal_v2
+#Ethan Montgomery
+
+"""
+#### CHANGE LOG ####
+V.10 - App Jar
+V.20 - Created base of the game
+###################
+"""
+
 # import the library
 from appJar import gui
 
-############# VARIABLES #####################
+### VARIABLES ###
 userName = ""
 questions = []
 quizProgress = 0
 score = 0
 
-######## CLASS DEFINITITIONS ################
-
+### CLASS DEFINITITIONS ###
 class Question:
     def __init__(self, questionText, options, answer):
         self.questionText = questionText
@@ -16,87 +25,29 @@ class Question:
         self.answer = answer
 
     def displayQuestion(self):
-        buttons = ["A","B","C","D","E"]
-        app.removeAllWidgets()
-        app.addLabel("questionText",self.questionText)
-        app.setLabelFg("questionText", "white")
-        app.addButton("Next",buttonFunctions)
-        app.hideButton("Next")
-        i=0
-       
-        for option in self.options:
-            app.addButton(buttons[i],self.getAnswer)
-            app.setButton(buttons[i],option)
-            i += 1
-           
-   
+        pass
+    
     def getAnswer(self, button):
-        global score
-        buttons = ["A","B","C","D","E"]
-        i = 0
-        for option in self.options:
-            app.hideButton(buttons[i])
-            i += 1
-        if button == self.answer:
-            app.setLabel("questionText", "Correct")
-            score +=1
-        else:
-            app.setLabel("questionText", "Incorrect")
-        app.showButton("Next")
-
-
-
-
-############# FUNCTIONS #######################
+        pass
+    
+### FUNCTIONS ###
 
 # handle button events
 def press(button):
-    global userName
-    if button == "Cancel":
-        app.stop()
-    else:
-        userName = app.getEntry("Name: ").capitalize()
-        start()
-   
+    pass
+
 def buttonFunctions(button):
-    global quizProgress
-    if button == "Start":
-        questions[0].displayQuestion()
-    if button == "Next":
-        if quizProgress < len(questions)-1:
-            quizProgress += 1
-            questions[quizProgress].displayQuestion()
-        else:
-            endScreen()
-    #print(button)
+    pass
 
 def start():
-    global userName
-    app.removeAllWidgets()
-    welcomeString = "Welcome "+str(userName)
-    app.addLabel("test", welcomeString, 0, 0)
-    app.addMessage("afda", "dad")
-    app.setLabelFg("test", "white")
-    app.addButton("Start",buttonFunctions)
-   
+    pass   
 
 def endScreen():
-    app.removeAllWidgets()
-    app.addLabel("finish","The Quiz Is Over, Your Score Is Below")
-    app.setLabelFg("finish", "white")
-    scoreString = str(score) +" / " +str(len(questions))
-    app.addLabel("score",scoreString)
-    app.setLabelFg("score", "white")
+    pass
 
-
-
-
-
-
-
- 
 # create a GUI variable called app
-app = gui("Login Window", "800x400")
+app = gui("Environment Quiz", "800x400")
+app.setResizable=False
 app.setBg("black")
 app.setFont(18)
 
@@ -113,19 +64,7 @@ app.addLabelEntry("Name: ", 2,0)
 app.setLabelFg("Name: ", "white")
 app.addButtons(["Submit", "Cancel"], press)
 
-################ QUESTIONS #####################
-
-
-questions.append(Question("Who am I?",["Josh","Ethan","Oliver"],"A"))
-questions.append(Question("Who are you (2)?",["Josh","Ethan","Oliver"],"B"))
-questions.append(Question("Who are you (3)?",["Josh","Ethan","Oliver"],"B"))
-
-
-
-
-
-
-
+### QUESTIONS ###
 
 # start the GUI
 app.go()
